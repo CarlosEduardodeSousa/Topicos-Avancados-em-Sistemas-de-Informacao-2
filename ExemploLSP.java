98fa01ee165c1a1c455a2c4291776041cfc9b0eb
Carlos Eduardo de Sousa <carloseduardosousa378@gmail.com>
2025-09-29 21:29:14 -0300

    Create ExemploLSP.java

---
+++ ExemploLSP.java
@@ -0,0 +1,31 @@
+package Solid;
+
+public class ExemploLSP {
+    //Interface para aves
+
+    static interface Ave {}
+
+    static interface AveQueVoa extends Ave{
+        void voar();
+    }
+
+    static class Pato implements AveQueVoa {
+        public void voar(){
+            System.out.println("Pato voando...");
+        }
+    }
+
+    static class Pinguim implements Ave{
+        public void nadar(){
+            System.out.println("Pinguim nadando...");
+        }
+    }
+
+    public static void main(String[] args) {
+        Pato pato = new Pato();
+        pato.voar();
+
+        Pinguim pinguim = new Pinguim();
+        pinguim.nadar();
+    }
+}
